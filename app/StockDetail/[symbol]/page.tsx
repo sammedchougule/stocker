e44b1f8f157eb6acc52834f56dcaab95c806137d
sammedchougule <smdchougule24@gmail.com>
2024-12-31 14:17:09 +0530

    useMemo DLT

--- app/StockDetail/[symbol]/page.tsx
+++ app/StockDetail/[symbol]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import {
   LineChart,
