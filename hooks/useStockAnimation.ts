8e2b599f3e19a2886559e105caf797e8aab79ea5
sammedchougule <smdchougule24@gmail.com>
2025-01-07 02:18:03 +0530

    ""

--- hooks/useStockAnimation.ts
+++ hooks/useStockAnimation.ts
@@ -1,28 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-export function useStockAnimation<T>(
-  currentValue: T | undefined,
-  compareKeys: (keyof T)[]
-): 'up' | 'down' | 'neutral' {
-  const [prevValue, setPrevValue] = useState<T | null>(null);
-  const [direction, setDirection] = useState<'up' | 'down' | 'neutral'>('neutral');
+export function useStockAnimation<T>(currentValue: T | undefined, compareKeys: (keyof T)[]) {
+  const prevValueRef = useRef<T | undefined>(undefined);
+  const [direction, setDirection] = useState<'up' | 'down' | null>(null);
 
   useEffect(() => {
+    const prevValue = prevValueRef.current;
     if (currentValue && prevValue) {
       for (const key of compareKeys) {
         if (currentValue[key] !== prevValue[key]) {
-          const isIncreasing = currentValue[key] > prevValue[key];
-          setDirection(isIncreasing ? 'up' : 'down');
+          setDirection(currentValue[key] > prevValue[key] ? 'up' : 'down');
           break;
         }
       }
-    } else if (!prevValue) {
-      // Handle initial render or reset direction
-      setDirection('neutral');
     }
-
-    setPrevValue(currentValue || null);
-  }, [currentValue, prevValue, compareKeys]);
+    prevValueRef.current = currentValue;
+  }, [currentValue, compareKeys]);
 
   return direction;
 }
