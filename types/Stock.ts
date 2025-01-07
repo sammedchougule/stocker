aa6aa4c7f5195c8dcbd64d03c1975443b888ba5d
sammedchougule <smdchougule24@gmail.com>
2025-01-07 19:29:34 +0530

    added all types

--- types/Stock.ts
+++ types/Stock.ts
@@ -1,22 +1,55 @@
-export interface Stock {
-  symbol: string;
-  companyname: string;
-  type: string;
-  exchange: string;
-  price: number;
-  change: number;
-  changepct: number;
-  high: number;
-  low: number;
-  high52: number;
-  low52: number;
-  marketcap: number;
-  pe: number | null;
-  industry: string;
-  sector: string;
-  volumespike?: number;
-  closings: { [date: string]: number };
-  indices?: { [key: string]: boolean };
-}
+export interface Indices {
+    "Nifty 50": boolean;
+    "Nifty FnO": boolean;
+    "Nifty Auto": boolean;
+    "Nifty Bank": boolean;
+    "Nifty Financial Services": boolean;
+    "Nifty FMCG": boolean;
+    "Nifty Healthcare": boolean;
+    "Nifty IT": boolean;
+    "Nifty Media": boolean;
+    "Nifty Metal": boolean;
+    "Nifty Pharma": boolean;
+    "Nifty PVT Bank": boolean;
+    "Nifty PSU Bank": boolean;
+    "Nifty Realty": boolean;
+  }
+  
+  export interface Stock {
+    symbol: string;
+    companyname: string;
+    industry: string | null;
+    sector: string | null;
+    viewchart: string | null;
+    indices: Indices | null;
+    type: string | null;
+    exchange: string | null;
+    currency: string | null;
+    closeyest: number;
+    priceopen: number;
+    price: number;
+    low: number;
+    high: number;
+    change: number;
+    changepct: number;
+    tradetime: string | null;
+    volume: number | null;
+    volumeavg: number | null;
+    volumespike: number | null;
+    month_high: number;
+    month_low: number;
+    high52: number;
+    low52: number;
+    marketcap: number | null;
+    eps: number | null;
+    pe: number | null;
+    shares: number | null;
+    closings: { [date: string]: number };
+  }
+  
+  export interface StockData {
+    data: Stock[];
+  }
+  
 
 export type SortOption = 'changepct_desc' | 'changepct_asc' | 'volumespike_desc' | 'volumespike_asc'; 
