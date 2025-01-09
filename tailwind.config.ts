0eeec833c36e9b81518bd3780141029c1a383306
sammedchougule <smdchougule24@gmail.com>
2025-01-09 13:50:57 +0530

    "eslint"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 
 export default {
   content: [
@@ -15,9 +16,6 @@ export default {
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide'),
+    tailwindScrollbarHide,
   ],
 } satisfies Config;
-
-
-
