39d2e1ad59b56191c99e304f8b46f249747aa0b6
sammedchougule <smdchougule24@gmail.com>
2025-01-09 14:34:01 +0530

    api added

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
-
+import { SubscriptionPlans } from '@/components/SubscriptionPlans'
 import './globals.css'
 import { StockProvider } from '@/context/StockContext'
 import Indices from '@/components/Indices'
 import TodayNews from '@/components/TodayNews'
 import TodaysStocks from '@/components/TodayStocks'
-import { SubscriptionPlans } from '@/components/SubscriptionPlans'
 
-export default  function Home() {
-  
+export const revalidate = 60 // Revalidate every 60 seconds
+
+async function fetchStockData() {
+  const res = await fetch(
+    'https://script.google.com/macros/s/AKfycbwa3ZVL20X9vlqFfpi6KSteUsEecC9QpkY3V45sxVAmEQ5xeBBKSaCUyQejxrRbwE6wGw/exec'
+  )
+  const stockData = await res.json()
+  return {
+    stocks: stockData.data,
+    lastUpdated: new Date().toISOString(),
+  }
+}
+
+export default async function Home() {
+  const initialData = await fetchStockData()
 
   return (
-    <StockProvider>
+    <StockProvider initialData={initialData}>
         <Indices />
         <TodaysStocks />
         <TodayNews />
