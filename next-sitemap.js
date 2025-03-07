530f392a10c7332afbafa670683985c2be80edc8
sammedchougule <smdchougule24@gmail.com>
2025-03-07 14:14:23 +0530

    added next-sitemap.js file

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://stocker.co.in', // Your website URL
+    generateRobotsTxt: true, // Automatically generates robots.txt file
+    sitemapSize: 7000, // Maximum number of URLs per sitemap
+  };
+  
