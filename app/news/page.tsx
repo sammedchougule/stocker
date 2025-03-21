8e16edd260fde5a8273652eae0b5629e9bbaca4b
sammedchougule <smdchougule24@gmail.com>
2025-03-21 21:40:51 +0530

    category

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,8 +5,6 @@ import News from "@/components/News";
 import { useSearchParams } from "next/navigation";
 
 export default function NewsPage() {
-  const searchParams = useSearchParams();
-  const category = searchParams.get("category") || "general";
 
   return (
     <div className="container mx-auto ">
