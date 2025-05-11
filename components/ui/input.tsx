bbfd2b51b519dc5d278a83de13ffc33b8b598047
sammedchougule <smdchougule24@gmail.com>
2025-05-12 00:50:18 +0530

    input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,9 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  isInvalid?: boolean
+}
 
 const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
@@ -19,7 +21,4 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
     )
   },
 )
-
 Input.displayName = "Input"
-
-export { Input }
